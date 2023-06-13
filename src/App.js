b2c7b84ab98190dccac600e202d307f3e0e615a6
Warren Pope <warrenpope2010@gmail.com>
2023-06-14 01:58:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react'
 
 function App() {
+  let initImage = null
+  const [image, setImage] = useState(null)
+
+  const loadImage = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="flex justify-between items-center bg-gray-800 px-5 md:px-10 h-[50px]">
+      <div className="text-2xl text-white font-bold">Picraft</div>
     </div>
+    <div className="h-[calc(100vh-100px)] bg-gray-100">
+      {
+        image ? (
+          <></>
+        ) : (
+          <div className="flex justify-center items-center h-full w-full">
+            <div></div>
+          </div>
+        )
+      }
+    </div>
+    <div className="flex items-center justify-center bg-gray-800 px-5 md:px-10 h-[50px]">
+
+    </div>
+    </>
   );
 }
 
